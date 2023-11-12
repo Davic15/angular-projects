6ca9fd37f8e85eaceeec662bc84b73b1620a3fbe
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-11-12 14:15:33 +0100

    weather application

---
+++ weather-app/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "./services/weather.service";
+import {WeatherData} from "./models/weather.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  cityName: string = 'Rome';
+  weatherData?: WeatherData;
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response)
+      }
+    })
+  }
+
+}
---
+++ weather-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  appid: '0a56dc1de13ff9072d70706723096a71'
+}
