f7c7deaeb66f6cebcf5111aa235ff98570096911
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2026-09-13 13:48:06 -0500

    fix(security): remove exposed NewsAPI key from newsweb project

--- newsweb/src/environments/environment.example.ts
+++ newsweb/src/environments/environment.example.ts
@@ -3,5 +3,5 @@ export const environment = {
   baseUrlHome: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=',
   baseUrlTech: 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=',
   baseUrlBusiness: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=',
-  appid: 'a4fc11170f0340ceb30d2955902338a3'
-}
+  appid: 'YOUR_NEWS_API_KEY'
+};
